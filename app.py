ef7b0d2383ef52f658b13d26f54ceed3e6897951
Karthik <karthik_in@rocketmail.com>
2019-06-24 21:42:14 +0530

    Python Basics

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import math
+
 print('Hello Python.')
 print('My Learning code samples for Python Programming')
 
@@ -11,3 +13,99 @@ print(greeting + ' likes ' + fav_color)
 weight_lbs = input('Enter your weight in pounds ')
 weight_kgs = float(weight_lbs) * 0.4535
 print(f'Your weight in Kilograms:{weight_kgs} kgs')
+
+course = 'Bob is a software developer'
+diff_var = course[:]
+diff_var += ' with 15 years experience.'
+print(diff_var)
+print(course)
+
+# Formatted Strings
+first = 'Hello'
+second = 'World!!!'
+message = f'{first}, {second} is my first program for formatted strings'
+print(message)
+
+# String Methods
+msg = 'Sample message to explain the String methods'
+print(f'The length of msg string is {len(msg)}')
+print(f'To Upper: {msg.upper()}')
+print(f'To Lower: {msg.lower()}')
+print(f'To Title: {msg.title()}')
+print(msg)
+print(msg.find('e'))
+print(msg.find('M'))
+print(msg.replace('message', 'Message'))
+print('message' in msg)  # in operator returns boolean
+print('Explain' in msg)  # in operator returns boolean
+
+# Arithmetic Operations
+print(9 // 4)  # returns the Quotient
+print(9 % 4)  # returns the Remainder
+print(5 ** 3)  # returns the Exponential value 5*5*5
+
+# Math Functions
+print(math.floor(3.5))
+print(math.ceil(4.2))
+print(math.factorial(5))
+
+# If Statements
+lp = 12
+if lp > 10:
+    print('Greater than 10')
+    print('Greater than 10 again')
+elif lp == 10:
+    print('Value is 10')
+else:
+    print('Lesser than 10')
+
+is_buyer_credit_good = False
+
+if is_buyer_credit_good:
+    print(f'Please pay 10% down payment which is:{1000000 * 0.1}')
+else:
+    print(f'Please pay 20% down payment which is:{1000000 * 0.2}')
+
+# Logical Operators AND, OR , NOT
+
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print('Eligible for loan')
+
+has_good_credit = False
+
+if has_high_income or has_good_credit:
+    print('Still Eligible for Loan')
+
+has_good_credit = True
+has_criminal_record = False
+if has_good_credit and not has_criminal_record:
+    print('No Criminal Record. So, Eligible for Loan')
+
+# Comparison Operators
+name = 'Arnold Schwarzeneggar'
+if len(name) < 5:
+    print('Name is too short')
+elif len(name) > 30:
+    print('Name is too long')
+else:
+    print('Name is valid')
+
+# Weight Converter
+weight = input('Weight:')
+pounds_or_kgs = input('(L)bs or (K)gs:')
+if pounds_or_kgs.upper() == "L":
+    print(f' Your Weight: {float(weight) * 0.45} kgs')
+elif pounds_or_kgs.upper() == "K":
+    print(f' Your Weight:{float(weight) / 0.45} lbs')
+else:
+    print('Invalid Input')
+
+# While Loop
+
+counter = 1
+while counter <= 10:
+    print(f"The While Loop value is: {counter}")
+    counter = counter + 1
