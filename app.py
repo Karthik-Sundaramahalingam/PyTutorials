5bb367bbca364c2c65624ef7d713f4692d8c2f91
Karthik <karthik_in@rocketmail.com>
2019-06-24 20:19:53 +0530

    Lesson 1 - print() and variables

---
+++ app.py
@@ -0,0 +1,13 @@
+print('Hello Python.')
+print('My Learning code samples for Python Programming')
+
+name = 'Sachin'
+age = 42
+is_new = False
+greeting = input('What''s your name? ')
+fav_color = input('What is your favorite color? ')
+print(greeting + ' likes ' + fav_color)
+
+weight_lbs = input('Enter your weight in pounds ')
+weight_kgs = float(weight_lbs) * 0.4535
+print(f'Your weight in Kilograms:{weight_kgs} kgs')
