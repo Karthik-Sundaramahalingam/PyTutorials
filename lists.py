21271075257612f39761f4551b8640e887fab492
Karthik-Sundaramahalingam <karthik_in@rocketmail.com>
2019-07-16 23:24:47 +0530

    Lists functions

--- lists.py
+++ lists.py
@@ -33,3 +33,25 @@ print('Popping a Racing Team from the Formula 1 list using pop() method')
 popped_team = formula_1.pop()
 print(formula_1)
 print(popped_team)
+
+print('Popping a Racing Team from the Formula 1 list using pop() method with an index')
+popped_team = formula_1.pop(2)
+print(formula_1)
+print(popped_team)
+
+print('Removing a Racing Team from the Formula 1 list using remove() method')
+team = 'Ferrari'
+formula_1.remove(team)
+print(formula_1)
+print(f'{team} has been removed')
+
+numbers = [3, 5, 9, 1, 4, 2, 6, 8, 7]
+print(numbers)
+numbers.sort()
+print(f'After Sorting: {numbers}')
+
+alphabets = ['B', 'D', 'A', 'C']
+print(alphabets)
+print('Temporarily Sorting using Sorted function')
+print(sorted(alphabets))
+print(f'Again back to Original List: {alphabets}')
